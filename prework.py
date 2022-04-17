589c4470cc1f8e520a966070dd56228917327dfd
yveenkwen <yveenkwen@gmail.com>
2022-04-17 13:38:59 -0700

    Question 4

--- prework.py
+++ prework.py
@@ -31,3 +31,17 @@ def max_num_in_list(a_list):
 
 a_list =[23, 45 , 2 , 43, 1, 7]
 print(max_num_in_list(a_list))
+
+
+# QUESTION 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4
+# but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+
+def is_leap_year(a_year):
+    if (a_year % 4 == 0 and a_year % 100 != 0 ) or ( a_year % 400 ==0) :
+        return True
+
+    return False
+    
+print(is_leap_year(2045))
