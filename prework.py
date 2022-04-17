683e0722627bed166d01b9c9f7a606ccc1986ee7
yveenkwen <yveenkwen@gmail.com>
2022-04-17 12:58:17 -0700

    Question 2

--- prework.py
+++ prework.py
@@ -6,3 +6,12 @@ def hello_name(user_name):
 
 
 hello_name("hello_USERNAME!")
+
+
+# Question 2
+# Write a python function, first_odds that print the odd numbers from 1-100 and returns nothing
+def first_odds():
+    for i in range (1,100,2):
+        print(i)
+
+first_odds()
